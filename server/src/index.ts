88cda3ecf45ef7187e121b4843b7e3db8d6bfd6a
Марія Мунтіян <maria-mun@users.noreply.github.com>
2024-12-05 16:50:30 +0200

    Add API for GET and POST book data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,49 @@
 import express from 'express';
-const app = express();
-
-import dotenv from 'dotenv';
-dotenv.config();
-
 import { Request, Response } from 'express';
-
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { Book } from './models/Book';
+
+const app = express();
+dotenv.config();
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+app.use(express.json());
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('<a href="/books">My books</a>');
+});
+
+app.get('/books', async (req: Request, res: Response) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ error: 'Помилка при отриманні книг' });
+  }
+});
+
+app.post('/books', async (req: Request, res: Response) => {
+  const newBook = new Book(req.body);
+  const createdBook = await newBook.save();
+  res.status(200).json(createdBook);
+});
+
+app.get('/books/:id', async (req: Request, res: Response) => {
+  const bookId = req.params.id;
+  try {
+    const book = await Book.findById(bookId);
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: 'Помилка при отриманні книги' });
+  }
+});
 
 mongoose.connect('mongodb://localhost/library_app_DB').then(() => {
-  console.log('connected to db and listening on port 3000');
-  app.listen(process.env.PORT || 3000);
+  console.log(`connected to db and listening on port ${process.env.PORT}`);
+  app.listen(process.env.PORT);
 });
